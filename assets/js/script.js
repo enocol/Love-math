7fdba99acc676983abba5fba1d63595bee14941c
enocol <eno_col@yahoo.com>
2024-01-31 10:21:15 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,26 @@ document.addEventListener("DOMContentLoaded", function(){
                alert("You clicked Submit!");
            } else {
                const gameType = this.getAttribute("data-type");
-               alert(`You clicked ${gameType}`);
+              runGame(gameType);
            }
         });
     }
 });
 
-function runGame() {
+function runGame(gameType) {
   const num1 = Math.floor(Math.random() * 25) + 1;
   const num2 = Math.floor(Math.random() * 25) + 1; 
+  
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting!`;
+    }
+}
+
+function displayAdditionQuestion(num1, num2) {
+    document.getElementById("operand1").textContent = num1;
+    document.getElementById("operand2").textContent = num2;
+    document.getElementById("operator").textContent = "+";
 }
